23d246af9ba7f6fcb7fdbe69e3f45b13f6de90a1
ccwangelaine <wangelaine@berkeley.edu>
2020-10-11 15:57:41 -0700

    starter commit

---
+++ app/src/main/java/com/example/ramp/UserModel.java
@@ -0,0 +1,5 @@
+package com.example.ramp;
+
+// define the User class and what it stores e.g. username, password, email, user id
+public class UserModel {
+}
---
+++ app/src/main/java/com/example/ramp/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.ramp;
+
+// database functions like get or put by id etc.
+public class UserRepository {
+}
