fc0de1e4910c41a22d888bb6537ec0fdfed549d8
anthonyzhou-1 <62827838+anthonyzhou-1@users.noreply.github.com>
2020-10-17 01:07:47 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/ramp/Login.java
@@ -0,0 +1,101 @@
+package com.example.ramp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    EditText email, password;
+    Button login, register;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.Lemail);
+        password = findViewById(R.id.Lpassword);
+        login = findViewById(R.id.LoginBtn);
+        register = findViewById(R.id.RegisterBtn);
+        fAuth = FirebaseAuth.getInstance();
+
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailtxt = email.getText().toString().trim();
+                String passwordtxt = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(emailtxt)) {
+                    email.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(passwordtxt)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(emailtxt, passwordtxt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailtxt = email.getText().toString().trim();
+                String passwordtxt = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(emailtxt)) {
+                    email.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(passwordtxt)){
+                    password.setError("Password is required");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(emailtxt, passwordtxt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+}
