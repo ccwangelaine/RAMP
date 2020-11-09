37fc55e26a9963f27bfcd25ce47b0cfaa63712fd
ccwangelaine <wangelaine@berkeley.edu>
2020-11-08 17:58:22 -0800

    blank start accessing files

---
+++ app/src/main/java/com/example/ramp/StartAccessing.java
@@ -0,0 +1,14 @@
+package com.example.ramp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartAccessing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_accessing);
+    }
+}
