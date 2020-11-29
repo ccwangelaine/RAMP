f2ffa09ff0cb6e7b28fe37d75234b2be1383fc5b
Abi <abigail.lomibao7@berkeley.edu>
2020-11-29 12:32:39 -0800

    merge

--- app/src/androidTest/java/com/example/ramp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/ramp/MainActivityTest.java
@@ -1,12 +1,28 @@
 package com.example.ramp;
 
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-@RunWith(AndroidJUnit4ClassRunner)
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityTest {
 
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
 }
