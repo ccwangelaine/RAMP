d98cad570c6ebec0df4192bc94c2af08c7b399e5
Kitty Gu <kittyguz@berkeley.edu>
2020-11-29 17:37:14 -0600

    implemented start accessing page

--- app/src/androidTest/java/com/example/ramp/LoginTest.java
+++ app/src/androidTest/java/com/example/ramp/LoginTest.java
@@ -5,7 +5,10 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.*;
 
-import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import androidx.annotation.CheckResult;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.core.app.ActivityScenario;
 import org.junit.Test;
@@ -14,6 +17,25 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class LoginTest {
+    private String emailToBeTyped;
+    private String passwordToBeTyped;
+    private String preferredNameToBeTyped;
+    private String gender;
+    private String age;
+
+    @Rule
+    ActivityScenario activityScenario = ActivityScenario.launch(Login.class);
+
+    @Before
+    public void initValidString() {
+        //Specify the password and email you want to sign up with
+        emailToBeTyped = "kittyguz@berkeley.edu";
+        passwordToBeTyped = "www.12345";
+        preferredNameToBeTyped = "Kitty";
+        gender = "Female";
+        age = "25";
+    }
+
     @Test
     public void test_isActivityInView() {
         //check if the page, title, and button is displayed.
@@ -39,6 +61,34 @@ public class LoginTest {
                 .perform(click());
         onView(withId(R.id.RegisterPage))
                 .check(matches(isDisplayed()));
+
+        //On Register Page
+        onView(withId(R.id.Lemail))
+                .perform(typeText(emailToBeTyped));
+        onView(withId(R.id.Lpassword))
+                .perform(typeText(passwordToBeTyped));
+        onView(withId(R.id.confirmPassword))
+                .perform(typeText(passwordToBeTyped), closeSoftKeyboard());
+        onView(withId(R.id.SignupBtn))
+                .perform(click());
+
+        //On Page 6: Personal Information Modality Information
+        onView(withId(R.id.preferName))
+                .perform(typeText(preferredNameToBeTyped));
+        onView(withId(R.id.etgender))
+                .perform(typeText(gender));
+        onView(withId(R.id.etage))
+                .perform(typeText(age));
+        //choose modality
+        onView(withId(R.id.modalityBtn))
+                .perform(click());
+
+//        ListView listView = alertDialog.getListView();
+//        View child = listView.getChildAt(0);
+//        child.performClick();
+
+        onView(withId(R.id.NextBtn1))
+                .perform(click());
     }
 
     @Test
@@ -48,10 +98,12 @@ public class LoginTest {
                 .perform(click());
         onView(withId(R.id.RegisterPage))
                 .check(matches(isDisplayed()));
+
         onView(withId(R.id.backToSignin))
                 .perform(click()); //method 1
         //pressBack(); //method 2
         onView(withId(R.id.LoginPage))
                 .check(matches(isDisplayed()));
     }
+
 }
--- app/src/main/java/com/example/ramp/StartAccessing.java
+++ app/src/main/java/com/example/ramp/StartAccessing.java
@@ -2,13 +2,40 @@ package com.example.ramp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import android.os.Bundle;
 
-public class StartAccessing extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
+public class StartAccessing extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    Button startAccess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_accessing);
+
+        startAccess = findViewById(R.id.StartAccessBtn);
+        startAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAccess();
+            }
+        });
+
+        TextView textView = (TextView) findViewById(R.id.textViewSA);
+        textView.setText("Hi" + user.getDisplayName());
+    }
+
+    public void startAccess() {
+        Intent intent = new Intent(StartAccessing.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
